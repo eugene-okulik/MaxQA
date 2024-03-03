5574508e2291ede1b1978bc5173a3fd7b7cae2b2
kmvwork <kmvwork@yandex.ru>
2024-03-03 13:25:08 +0300

    Lesson_5

--- homework/maxim_ivanov/Lesson_5/1.py
+++ homework/maxim_ivanov/Lesson_5/1.py
@@ -7,13 +7,17 @@ text1 = 'результат операции: 42'
 text2 = 'результат операции: 514'
 text3 = 'результат работы программы: 9'
 
-text1_number = int(text1[text1.index('42'):])
-text2_number = int(text2[text2.index('514'):])
-text3_number = int(text3[text3.index('9'):])
+text1_pos = text1.index(':') + 1
+text2_pos = text2.index(':') + 1
+text3_pos = text3.index(':') + 1
 
-print(text1_number + 10)
-print(text2_number + 10)
-print(text3_number + 10)
+text1_num = int(text1[text1_pos:].strip())
+text2_num = int(text2[text2_pos:].strip())
+text3_num = int(text3[text3_pos:].strip())
+
+print(text1_num + 10)
+print(text2_num + 10)
+print(text3_num + 10)
 
 # Задание 3
 students = ['Ivanov', 'Petrov', 'Sidorov']
