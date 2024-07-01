e5c11f46b00721fa7ca30c0ff618cad95a67de92
kmvwork <kmvwork@yandex.ru>
2024-07-01 07:30:18 +0300

    Lesson_26

--- homework/maxim_ivanov/Lesson_26/test_1.py
+++ homework/maxim_ivanov/Lesson_26/test_1.py
@@ -4,6 +4,6 @@ from playwright.sync_api import Page, expect
 def test_get_started_link(page: Page):
     page.goto("https://the-internet.herokuapp.com/")
     page.get_by_role("link", name="Form Authentication").click()
-    page.get_by_label("Username").fill('MyLogin')
-    page.get_by_label("Password").fill('MyPassword')
+    page.get_by_role("textbox", name='username').fill('MyLogin')
+    page.get_by_role("textbox", name='password').fill('MyPassword')
     page.get_by_role("button", name=" Login").click()
